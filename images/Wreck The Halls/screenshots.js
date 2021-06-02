e6d974b25362057d2b106916851a1d504f485c23
Kenny Park <parkkennypark@gmail.com>
2021-06-01 23:00:08 -0700

    Updates

--- images/Wreck The Halls/screenshots.js
+++
@@ -1,8 +0,0 @@
-export default [
-  "screenshots/iPhone 5.52020-01-10_14-19-04.png",
-  "screenshots/iPhone 5.52020-01-10_14-23-10.png",
-  "screenshots/iPhone 5.52020-01-10_14-19-04.png",
-  "screenshots/iPhone 5.52020-01-10_14-19-04.png",
-  "screenshots/iPhone 5.52020-01-10_14-19-04.png",
-  "screenshots/iPhone 5.52020-01-10_14-19-04.png",
-];
